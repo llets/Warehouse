ffc331c5a2cef14792efb9544c44fa3811cd0f6a
llets <rizar_sasha@mail.ru>
2023-10-07 21:19:07 +0500

    Template for further work

--- server/index.js
+++ server/index.js
@@ -1,7 +1,26 @@
+require('dotenv').config()
 const express = require('express')
-
+const sequelize = require('./db')
 const PORT = process.env.PORT || 5000
+const cors = require('cors')
+const router = require('./routes/index')
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/api', router)
 
-app.listen(PORT, console.log(`Server started on PORT ${PORT}`))
+// обработка ошибок, последний middleware
+app.use(errorHandler)
+
+const start = async () => {
+    try{
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
+    } catch (e) {
+        console.log((e))
+    }
+}
+
+start()
