ceb3e1dc66f871fc0d2eacb930837fab577cafc6
Вадим Сухинин <suhinin.vadim2002@yandex.ru>
2023-10-07 22:23:29 +0500

    index.js - добавлена 4-я строка

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
+const models = require('./models/models')
 const PORT = process.env.PORT || 5000
 const cors = require('cors')
 const router = require('./routes/index')
